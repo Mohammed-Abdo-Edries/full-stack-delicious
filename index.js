c0b4d46c83b0b8ac8c0c3be714959d17e3a4a51c
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2022-10-29 05:48:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require ('path')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "first", "build")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("*", async(req, res) => {
+    res.sendFile(path.join(__dirname, "first", "build", "index.html"))
+    res.status(200);
+});
+
+app.listen(PORT,() =>{
+    console.log('listening on '+ PORT)
+})
